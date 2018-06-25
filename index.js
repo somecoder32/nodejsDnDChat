f54dc43b11f67e8ae44d0ac82ba014424538dae6
somecoder32 <thenewjason@live.com>
2018-06-24 22:03:59 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -14,9 +14,25 @@ io.on('connection', function(socket){
     socket.on('disconnect', function(){
       console.log('user disconnected');
     });
-    socket.on('chat message', function(msg, die){
-        if(die){
-            io.emit('chat message', msg + 'Rolls a d' + die + ' for ' +  Math.floor((Math.random() * die) + 1));
+    socket.on('chat message', function(msg, die){     
+        
+        var endMsg = msg.split(" ")[1];
+        var isNum = parseInt(endMsg, 10);
+
+        if(die){                       
+            if(isNum){
+                var rolls = roll(isNum, die);
+                var total = 0;
+
+                for(var i = 0; i < rolls.length; i++){
+                    io.emit('chat message', msg.split(" ")[0] + 'Rolls a d' + die + ' for ' +  rolls[i]);
+                    total = total + rolls[i];
+                }
+                io.emit('chat message', msg.split(" ")[0] + 'Total = ' + total);
+            }
+            else {
+                io.emit('chat message', msg + 'Rolls a d' + die + ' for ' +  Math.floor((Math.random() * die) + 1));
+            }
         }
         else {
             io.emit('chat message', msg);
@@ -27,3 +43,14 @@ io.on('connection', function(socket){
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+function roll(numdice, dicesize){
+    
+    var results = [];
+    
+    for(var i = 0; i < numdice; i++) {
+        results[i] =  Math.floor((Math.random() * dicesize) + 1);
+    }
+    
+    return results;
+}
